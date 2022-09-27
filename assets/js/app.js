c3975097b233fbbb0a14aabf3808520513dfde81
Musharrof Shishir <musharrofshishir@gmail.com>
2022-09-27 09:21:06 +0600

    from new mac

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,3 +8,18 @@ const date ={
 }
 // GSAP
 gsap.registerPlugin(ScrollTrigger);
+// marque animation
+// gsap.fromTo('._real_marque_content_ul',{
+//   // scrollTrigger:{
+//   //   trigger:'._real_marque_content_wrap',
+//   //   scrub: .2,
+//   // },
+//   x: -330,
+//   duration:3,
+//   ease: "linear",
+// },{
+//   x: 20,
+//   duration:3,
+//   ease: "linear",
+//   repeat: -1
+// });
