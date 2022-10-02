d40f7c85ca8c82d6238d6938807039b64767408f
Musharrof Shishir <musharrofshishir@gmail.com>
2022-10-02 09:37:30 +0600

    marquee animation

--- assets/js/app.js
+++ assets/js/app.js
@@ -29,17 +29,17 @@ new Splide( '._rylic_testimonial_slider',{
 // GSAP
 gsap.registerPlugin(ScrollTrigger);
 // marque animation
-// gsap.fromTo('._real_marque_content_ul',{
-//   // scrollTrigger:{
-//   //   trigger:'._real_marque_content_wrap',
-//   //   scrub: .2,
-//   // },
-//   x: -330,
-//   duration:3,
-//   ease: "linear",
-// },{
-//   x: 20,
-//   duration:3,
-//   ease: "linear",
-//   repeat: -1
-// });
+gsap.fromTo('._real_marque_content_ul',{
+  // scrollTrigger:{
+  //   trigger:'._real_marque_content_wrap',
+  //   scrub: .2,
+  // },
+  x: -330,
+  duration:3,
+  ease: "linear",
+},{
+  x: 20,
+  duration:3,
+  ease: "linear",
+  repeat: -1
+});
